7185895a9e6c53fd772d9be1646d9b7521ad0c5a
Fernando Zamora <fernandozamorarico@gmail.com>
2023-11-03 12:26:15 +0100

    Subiendo Repositorio

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express') //importamos express
 // creamos un objeto con la funcion express que contiene métodos
 // para hacer peticiones e iniciar el servidor
-const app = express() 
+const app = express();
+require('dotenv').config();
 // const port = 3000
 const userRoutes = require("./routes/users");
+
+// Database conection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_CNN);
+  console.log('Database connected ')
+}
+main().catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/users',userRoutes);
@@ -11,6 +22,6 @@ app.use('/users',userRoutes);
 // Iniciamos el servidor
 app.listen(3000, function() {
   console.log(
-    "El servidor se ha iniciado en el puerto 3000. ¡Ve a localhost:3000 en el navegador y mira lo que hay!"
+    `El servidor se ha iniciado en el puerto ${process.env.PORT}`
   );
 })
